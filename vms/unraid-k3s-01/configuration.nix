b065d8b72b750de9ae7e9e11ca39a7fc48db34e8
Thales Menato <8753631+thamenato@users.noreply.github.com>
2024-08-22 16:05:00 -0400

    feat: add zoth-ommog pub key

--- vms/unraid-k3s-01/configuration.nix
+++ vms/unraid-k3s-01/configuration.nix
@@ -50,6 +50,7 @@
     hashedPassword = "$6$U/Gk7/zD4uUWVGzJ$CJ6ZKPLpBCUUVzmwsRlv2csJbIuChM8pf1mlRIdazdQBvQyCS3uukcKwH0t20WqJKmDOdQB2N5.qc5TYbKwn01";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGnbZosCtInrhlAvKxwwDITIRqwGcCsCuR8E2FA4dwKh thamenato@kassogtha"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM+Xfrcniquxk540pVUPSxSj4vyBrCZbbjmRQkl3dvQX thamenato@zoth-ommog"
     ];
   };
 
